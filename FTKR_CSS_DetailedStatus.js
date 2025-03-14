626089e58aa25c9b36cd507a4e0a8e97e598cf3e
Leooo <kpoha.leonide91@gmail.com>
2025-03-14 07:20:01 +0100

    Update FTKR_CSS_DetailedStatus.js

--- FTKR_CSS_DetailedStatus.js
+++ FTKR_CSS_DetailedStatus.js
@@ -1,12 +1,12 @@
-//=============================================================================
-// 詳細ステータス画面の表示内容を変更するプラグイン
+// =============================================================================
+// Plugin to modify the display content of the detailed status screen
 // FTKR_CSS_DetailedStatus.js
-// プラグインNo : 27
-// 作成者     : フトコロ
-// 作成日     : 2017/04/21
-// 最終更新日 : 2018/12/13
-// バージョン : v2.1.4
-//=============================================================================
+// Plugin No : 27
+// Author     : Futokoro
+// Creation Date : 2017/04/21
+// Last Updated Date : 2018/12/13
+// Version : v2.1.4
+// =============================================================================
 
 var Imported = Imported || {};
 Imported.FTKR_CSS_DS = true;
@@ -14,216 +14,224 @@ Imported.FTKR_CSS_DS = true;
 var FTKR = FTKR || {};
 FTKR.CSS.DS = FTKR.CSS.DS || {};
 
-//=============================================================================
+// =============================================================================
 /*:
- * @plugindesc v2.1.4 詳細ステータス画面の表示内容を変更するプラグイン
- * @author フトコロ
+ * @plugindesc v2.1.4 Plugin to modify the display content of the detailed status screen
+ * @author Futokoro
  *
- * @param --詳細ステータスの表示設定--
+ * @param --Detailed Status Display Settings--
  * @default
  * 
  * @param statusList
- * @desc 表示するステータスとその位置を設定します。
+ * @desc Sets the statuses to display and their positions.
  * @type struct<status>[]
  * @default ["{\"text\":\"name\",\"value\":\"\",\"x\":\"6\",\"y\":\"0\",\"width\":\"150\"}","{\"text\":\"class\",\"value\":\"\",\"x\":\"192\",\"y\":\"0\",\"width\":\"150\"}","{\"text\":\"nickname\",\"x\":\"432\",\"y\":\"0\",\"width\":\"150\"}","{\"text\":\"line\",\"value\":\"\",\"x\":\"0\",\"y\":\"36\",\"width\":\"780\"}","{\"text\":\"face\",\"x\":\"12\",\"y\":\"72\",\"width\":\"144\"}","{\"text\":\"name\",\"x\":\"204\",\"y\":\"72\",\"width\":\"150\"}","{\"text\":\"state\",\"x\":\"204\",\"y\":\"108\",\"width\":\"150\"}","{\"text\":\"hp\",\"x\":\"204\",\"y\":\"144\",\"width\":\"150\"}","{\"text\":\"mp\",\"x\":\"204\",\"y\":\"180\",\"width\":\"150\"}","{\"text\":\"custom(%1)\",\"value\":\"0\",\"x\":\"456\",\"y\":\"72\",\"width\":\"270\"}","{\"text\":\"custom(%1)\",\"value\":\"1\",\"x\":\"456\",\"y\":\"108\",\"width\":\"270\"}","{\"text\":\"custom(%1)\",\"value\":\"2\",\"x\":\"456\",\"y\":\"144\",\"width\":\"270\"}","{\"text\":\"custom(%1)\",\"value\":\"3\",\"x\":\"456\",\"y\":\"180\",\"width\":\"270\"}","{\"text\":\"line\",\"value\":\"\",\"x\":\"0\",\"y\":\"216\",\"width\":\"780\"}","{\"text\":\"param(%1)\",\"value\":\"2\",\"x\":\"48\",\"y\":\"252\",\"width\":\"160\"}","{\"text\":\"equip(%1)\",\"value\":\"0\",\"x\":\"432\",\"y\":\"252\",\"width\":\"312\"}","{\"text\":\"param(%1)\",\"value\":\"3\",\"x\":\"48\",\"y\":\"288\",\"width\":\"160\"}","{\"text\":\"equip(%1)\",\"value\":\"1\",\"x\":\"432\",\"y\":\"288\",\"width\":\"312\"}","{\"text\":\"param(%1)\",\"value\":\"4\",\"x\":\"48\",\"y\":\"324\",\"width\":\"160\"}","{\"text\":\"equip(%1)\",\"value\":\"2\",\"x\":\"432\",\"y\":\"324\",\"width\":\"312\"}","{\"text\":\"param(%1)\",\"value\":\"5\",\"x\":\"48\",\"y\":\"360\",\"width\":\"160\"}","{\"text\":\"equip(%1)\",\"value\":\"3\",\"x\":\"432\",\"y\":\"360\",\"width\":\"312\"}","{\"text\":\"param(%1)\",\"value\":\"6\",\"x\":\"48\",\"y\":\"396\",\"width\":\"160\"}","{\"text\":\"equip(%1)\",\"value\":\"5\",\"x\":\"432\",\"y\":\"396\",\"width\":\"312\"}","{\"text\":\"param(%1)\",\"value\":\"7\",\"x\":\"48\",\"y\":\"432\",\"width\":\"160\"}","{\"text\":\"equip(%1)\",\"value\":\"4\",\"x\":\"432\",\"y\":\"432\",\"width\":\"312\"}","{\"text\":\"line\",\"value\":\"\",\"x\":\"0\",\"y\":\"468\",\"width\":\"780\"}","{\"text\":\"profile\",\"value\":\"\",\"x\":\"0\",\"y\":\"504\",\"width\":\"780\"}"]
+ * @parent --Detailed Status Display Settings--
  * 
  * @param DS Space In Text
- * @desc Text内で複数表示する場合の間隔を指定します。
+ * @desc Specifies the interval when displaying multiple items within the text.
  * @default 5
+ * @parent --Detailed Status Display Settings--
  * 
- * @param --ウィンドウ設定--
+ * @param --Window Settings--
  * @desc 
  * 
  * @param Enabled Custom Window
- * @desc ウィンドウのレイアウト変更機能を使うか。
- * 0 - 無効, 1 - 有効
+ * @desc Whether to use the window layout change feature.
+ * 0 - Disabled, 1 - Enabled
  * @default 0
  * @type select
- * @option 無効にする
+ * @option Disable
  * @value 0
- * @option 有効にする
+ * @option Enable
  * @value 1
+ * @parent --Window Settings--
  * 
  * @param Font Size
- * @desc フォントサイズ
+ * @desc Font size
  * @default 28
+ * @parent --Window Settings--
  * 
  * @param Window Padding
- * @desc ウィンドウの周囲の余白
+ * @desc Padding around the window
  * @default 18
+ * @parent --Window Settings--
  * 
  * @param Window Line Height
- * @desc ウィンドウ内の1行の高さ
+ * @desc Line height within the window
  * @default 36
+ * @parent --Window Settings--
  * 
  * @param Window Opacity
- * @desc ウィンドウ内の背景の透明度
+ * @desc Background opacity of the window
  * @default 192
+ * @parent --Window Settings--
  * 
  * @param Hide Window Frame
- * @desc ウィンドウ枠を非表示にするか
- * 1 - 非表示にする、0 - 表示する
+ * @desc Whether to hide the window frame
+ * 1 - Hide, 0 - Show
  * @default 0
  * @type select
- * @option 表示する
+ * @option Show
  * @value 0
- * @option 非表示にする
+ * @option Hide
  * @value 1
+ * @parent --Window Settings--
  * 
  * @param Auto Refresh Window
- * @desc 表示内容の自動更新に関する設定
- * この設定は、レイアウト変更が無効でも効果があります
+ * @desc Settings for automatically refreshing the display content
+ * This setting will work even if layout changes are disabled
  * @type struct<update>
  * @default {"enabled":"false","count":"60"}
+ * @parent --Window Settings--
  * 
  * @help
- *-----------------------------------------------------------------------------
- * 概要
- *-----------------------------------------------------------------------------
- * 本プラグインを実装することで、詳細ステータス画面のレイアウトを変更できます。
+ * -----------------------------------------------------------------------------
+ * Overview
+ * -----------------------------------------------------------------------------
+ * This plugin allows you to change the layout of the detailed status screen.
  * 
- * このプラグインには、FTKR_CustomSimpleActorStatus.js (v3.0.0以降)が必要です。
+ * This plugin requires FTKR_CustomSimpleActorStatus.js (v3.0.0 or later).
  * 
- * プラグインの使い方は、下のオンラインマニュアルページを見てください。
- * https://github.com/futokoro/RPGMaker/blob/master/FTKR_CSS_DetailedStatus.ja.md
+ * To use this plugin, please refer to the online manual below:
+ * https://github.com/futokoro/RPGMaker/blob/master/FTKR_CSS_DetailedStatus.en.md
  * 
  * 
- *-----------------------------------------------------------------------------
- * 設定方法
- *-----------------------------------------------------------------------------
- * 1.「プラグインマネージャー(プラグイン管理)」に、本プラグインを追加して
- *    ください。
+ * -----------------------------------------------------------------------------
+ * Setup Instructions
+ * -----------------------------------------------------------------------------
+ * 1. Add this plugin to the "Plugin Manager" of your project.
  * 
- * 2. 以下のプラグインと組み合わせる場合は、プラグイン管理の順番に注意してください。
+ * 2. When combining with the following plugins, pay attention to the order in the Plugin Manager.
  * 
- *    FTKR_CustomSimpleActorStatus.js (ステータス表示を変更)
- *    ↑このプラグインよりも上に登録↑
+ *    FTKR_CustomSimpleActorStatus.js (Modifies the status display)
+ *    ↑ Register above this plugin ↑
  *    FTKR_CSS_DetailedStatus.js
  * 
  * 
- *-----------------------------------------------------------------------------
- * アクターの詳細ステータス表示の設定
- *-----------------------------------------------------------------------------
- * プラグインパラメータの設定により、ステータス画面で表示する表示レイアウトを
- * 変更することができます。
+ * -----------------------------------------------------------------------------
+ * Actor Detailed Status Display Settings
+ * -----------------------------------------------------------------------------
+ * By configuring the plugin parameters, you can modify the layout of the status screen.
  * 
- * 各パラメータの意味と、設定方法は、
- * FTKR_CustomSimpleActorStatus.jsのヘルプを参照してください。
+ * For the meaning of each parameter and how to configure them, refer to the help of
+ * FTKR_CustomSimpleActorStatus.js.
  * 
- * なお、歩行キャラ、SV戦闘キャラ、カスタムパラメータ、カスタムゲージの
- * 設定は、FTKR_CustomSimpleActorStatus.jsの設定に従います。
+ * Additionally, settings for walking characters, SV battle characters, custom parameters,
+ * and custom gauges will follow the configuration of FTKR_CustomSimpleActorStatus.js.
  * 
  * 
- *-----------------------------------------------------------------------------
- * ステータスウィンドウの設定
- *-----------------------------------------------------------------------------
- * 以下のプラグインパラメータで設定できます。
+ * -----------------------------------------------------------------------------
+ * Status Window Settings
+ * -----------------------------------------------------------------------------
+ * The following plugin parameters can be configured.
  * 
  * <Enabled Custom Window>
- *    :スキル画面のウィンドウ変更機能を使うか指定します。
- *    :0 - 無効, 1 - 有効
+ *    : Specifies whether to use the window layout change feature.
+ *    : 0 - Disabled, 1 - Enabled
  * 
  * <Font Size>
- *    :ウィンドウ内のフォントサイズを変更します。
- *    :デフォルトは 28 です。(単位はpixel)
+ *    : Changes the font size within the window.
+ *    : Default is 28. (Unit is pixels)
  * 
  * <Window Padding>
- *    :ウィンドウの周囲の余白を変更します。
- *    :デフォルトは 18 です。(単位はpixel)
+ *    : Changes the padding around the window.
+ *    : Default is 18. (Unit is pixels)
  * 
  * <Window Line Height>
- *    :ウィンドウ内の1行の高さを変更します。
- *    :デフォルトは 36 です。(単位はpixel)
+ *    : Changes the line height within the window.
+ *    : Default is 36. (Unit is pixels)
  * 
  * <Window Opacity>
- *    :ウィンドウ内の背景の透明度を変更します。
- *    :デフォルトは 192 です。
- *    :0 - 透明、255 - 不透明
+ *    : Changes the background opacity of the window.
+ *    : Default is 192.
+ *    : 0 - Transparent, 255 - Opaque
  * 
  * <Hide Window Frame>
- *    :ウィンドウ枠を非表示にするか指定します。
- *    :1 - 非表示にする、0 - 表示する
- *    :デフォルトは表示します。
+ *    : Specifies whether to hide the window frame.
+ *    : 1 - Hide, 0 - Show
+ *    : Default is Show.
  * 
  * 
- * ＜ウィンドウの高さ＞
- * ウィンドウの高さは、以下の計算式で算出します。
- *    [ウィンドウ高さ] ＝ [縦の行数] × [1行の高さ] + [余白のサイズ] × 2
+ * <Window Height>
+ * The window height is calculated by the following formula:
+ *    [Window Height] = [Number of Vertical Rows] × [Line Height] + [Padding Size] × 2
  * 
  * 
- * ＜フォントサイズと行の高さ＞
- * 基本的に、下の大小関係になるように設定しましょう。
- *    フォントサイズ ＜ 1行の高さ
+ * <Font Size and Line Height>
+ * It is recommended to set the following relationships:
+ *    Font Size < Line Height
  * 
  * 
- * ＜ウィンドウを消す方法＞
- * 以下の設定にすると、ウィンドウ枠とウィンドウの背景が消えて
- * アクターのステータスだけを表示します。
+ * <How to Hide the Window>
+ * The following settings will hide the window frame and background,
+ * displaying only the actor's status.
  * 
  * <Window Opacity>     : 0
  * <Hide Window Frame>  : 1
  * 
  * 
- *-----------------------------------------------------------------------------
- * 本プラグインのライセンスについて(License)
- *-----------------------------------------------------------------------------
- * 本プラグインはMITライセンスのもとで公開しています。
+ * -----------------------------------------------------------------------------
+ * License Information for this Plugin
+ * -----------------------------------------------------------------------------
  * This plugin is released under the MIT License.
  * 
  * Copyright (c) 2017,2018 Futokoro
  * http://opensource.org/licenses/mit-license.php
  * 
- * 
- * プラグイン公開元
+ * Plugin Repository:
  * https://github.com/futokoro/RPGMaker/blob/master/README.md
  * 
- *-----------------------------------------------------------------------------
- * 変更来歴
- *-----------------------------------------------------------------------------
- * 
- * v2.1.4 - 2018/12/13 : プラグインパラメータstatusListの初期値変更
- * 
- * v2.1.3 - 2018/10/20 : 競合回避
- *    1. dsWeaponMasteryプラグインの熟練度表示に対応
- * 
- * v2.1.2 - 2018/09/29 : 機能追加
- *    1. プラグインパラメータのリストで選択できる項目を追加。
- * 
- * v2.1.1 - 2018/09/12 : 不要なプラグインパラメータを削除
- * 
- * v2.1.0 - 2018/08/30 : 機能追加
- *    1. ウィンドウの表示内容を自動更新する機能を追加。
- *    2. プラグインパラメータで表示するステータスをリストで選択できる機能を追加。
- * 
- * v2.0.0 - 2018/08/19 : FTKR_CustomSimpleActorStatus v3.0.0 対応版に変更
- * 
- * v1.1.0 - 2017/11/18 : 仕様変更
- *    1. FTKR_CustomSimpleActorStatus.js の v2.6.0に対応。
- * 
- * v1.0.2 - 2017/05/13 : 不具合修正
- *    1. ウィンドウ設定が正常に機能していない不具合を修正。
- * 
- * v1.0.1 - 2017/05/08 : 機能追加、不要なパラメータを削除
- *    1. ウィンドウの設定変更機能を追加。
- * 
- * v1.0.0 - 2017/04/21 : 初版作成
- * 
- *-----------------------------------------------------------------------------
+ * -----------------------------------------------------------------------------
  */
-//=============================================================================
+
+/* ja:
+ * -----------------------------------------------------------------------------
+ * Changelog
+ * -----------------------------------------------------------------------------
+ * 
+ * v2.1.4 - 2018/12/13 : Changed default value for plugin parameter statusList
+ * 
+ * v2.1.3 - 2018/10/20 : Conflict avoidance
+ *    1. Supported proficiency display in dsWeaponMastery plugin
+ * 
+ * v2.1.2 - 2018/09/29 : Feature Addition
+ *    1. Added selectable items in the plugin parameter list.
+ * 
+ * v2.1.1 - 2018/09/12 : Removed unnecessary plugin parameters
+ * 
+ * v2.1.0 - 2018/08/30 : Feature Addition
+ *    1. Added auto-refresh function for the window's display content.
+ *    2. Added selectable statuses in the plugin parameter list.
+ * 
+ * v2.0.0 - 2018/08/19 : Changed to be compatible with FTKR_CustomSimpleActorStatus v3.0.0
+ * 
+ * v1.1.0 - 2017/11/18 : Spec Change
+ *    1. Compatible with FTKR_CustomSimpleActorStatus.js v2.6.0
+ * 
+ * v1.0.2 - 2017/05/13 : Bug Fix
+ *    1. Fixed bug where window settings weren't functioning correctly.
+ * 
+ * v1.0.1 - 2017/05/08 : Feature Addition, Removed Unnecessary Parameters
+ *    1. Added window setting change feature.
+ * 
+ * v1.0.0 - 2017/04/21 : Initial Release
+ * 
+ * -----------------------------------------------------------------------------
+ */
 /*~struct~status:
  * @param text
- * @desc 表示するステータスを選択
- * リストにない場合は、直接テキストで記述
+ * @desc Select the status to display
+ * If it's not in the list, write it directly as text
  * @default 
  * @type select
- * @option 名前
+ * @option Name
  * @value name
- * @option 二つ名
+ * @option Nickname
  * @value nickname
- * @option 職業
+ * @option Class
  * @value class
- * @option レベル
+ * @option Level
  * @value level
  * @option HP
  * @value hp
@@ -231,108 +239,108 @@ FTKR.CSS.DS = FTKR.CSS.DS || {};
  * @value mp
  * @option TP
  * @value tp
- * @option 顔画像
+ * @option Face Image
  * @value face
- * @option 顔画像(サイズ指定)
+ * @option Face Image (Size Specified)
  * @value face(%1)
- * @option 歩行キャラ画像
+ * @option Character Image
  * @value chara
- * @option SV戦闘キャラ画像
+ * @option SV Battle Character Image
  * @value sv
- * @option ステート(横)
+ * @option State (Horizontal)
  * @value state
- * @option ステート(縦)
+ * @option State (Vertical)
  * @value state2(%1)
- * @option プロフィール
+ * @option Profile
  * @value profile
- * @option 通常能力値
+ * @option Normal Parameter Value
  * @value param(%1)
- * @option 通常能力値(素)
+ * @option Normal Parameter Value (Base)
  * @value pbase(%1)
- * @option 通常能力値(増加分)
+ * @option Normal Parameter Value (Increased)
  * @value pdiff(%1)
- * @option 装備
+ * @option Equip
  * @value equip(%1)
- * @option 装備パラメータ
+ * @option Equip Parameter
  * @value eparam(%1)
- * @option カスタムパラメータ
+ * @option Custom Parameter
  * @value custom(%1)
- * @option カスタムゲージ
+ * @option Custom Gauge
  * @value gauge(%1)
- * @option アクター別カスタムゲージ
+ * @option Actor-Specific Custom Gauge
  * @value agauge(%1)
- * @option クラス別カスタムゲージ
+ * @option Class-Specific Custom Gauge
  * @value cgauge(%1)
- * @option カスタム画像
+ * @option Custom Image
  * @value image
- * @option カスタム画像(登録ID)
+ * @option Custom Image (Registered ID)
  * @value image(%1)
- * @option メッセージ
+ * @option Message
  * @value message
- * @option テキスト
+ * @option Text
  * @value text(%1)
- * @option JS計算式(数値表示)
+ * @option JS Formula (Numeric Display)
  * @value eval(%1)
- * @option JS計算式(文字列表示)
+ * @option JS Formula (String Display)
  * @value streval(%1)
- * @option 横線
+ * @option Horizontal Line
  * @value line
- * @option AOP能力値
+ * @option AOP Ability Value
  * @value aop(%1)
- * @option AOP能力値(素)
+ * @option AOP Ability Value (Base)
  * @value aopbase(%1)
- * @option AOP能力値(増加分)
+ * @option AOP Ability Value (Increased)
  * @value aopdiff(%1)
- * @option AOP装備パラメータ
+ * @option AOP Equip Parameter
  * @value eaop(%1)
- * @option アイテム名
+ * @option Item Name
  * @value iname
- * @option アイテムアイコン
+ * @option Item Icon
  * @value iicon
- * @option アイテム説明
+ * @option Item Description
  * @value idesc
- * @option アイテムタイプ
+ * @option Item Type
  * @value itype
- * @option アイテム装備タイプ
+ * @option Item Equip Type
  * @value ietype
- * @option アイテム範囲
+ * @option Item Range
  * @value iscope
- * @option アイテム属性
+ * @option Item Element
  * @value ielement
- * @option アイテム設定詳細
+ * @option Item Detailed Settings
  * @value iparam(%1)
- * @option アイテムカスタム画像
+ * @option Item Custom Image
  * @value iimage(%1)
- * @option マップ名
+ * @option Map Name
  * @value mapname
  *
  * @param value
- * @desc code(%1)の形式で設定するステータスの%1の内容を入力
+ * @desc Enter the content of %1 for the status set in code(%1) format
  * @default 
  * 
  * @param x
- * @desc 表示するX座標
+ * @desc X coordinate to display
  * @default 0
  *
  * @param y
- * @desc 表示するY座標
+ * @desc Y coordinate to display
  * @default 0
  *
  * @param width
- * @desc 表示する幅
+ * @desc Width to display
  * @default 0
  *
  */
 /*~struct~update:
  * @param enabled
- * @desc ウィンドウの表示を自動更新するか
+ * @desc Whether to auto-update the window's display
  * @default false
  * @type boolean
- * @on 自動更新ON
- * @off 自動更新OFF
+ * @on Auto-update ON
+ * @off Auto-update OFF
  * 
  * @param count
- * @desc 表示更新間隔を設定します。
+ * @desc Set the display update interval.
  * @default 60
  * @type number
  *
